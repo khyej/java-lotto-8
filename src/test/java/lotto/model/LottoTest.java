fcc291f9c8e30a084c2cd60c36cd3b9ceccb75e7
khyej <wl159753@naver.com>
2025-11-03 00:59:43 +0900

    test: 로또 번호 검증 기능 테스트

--- src/test/java/lotto/model/LottoTest.java
+++ src/test/java/lotto/model/LottoTest.java
@@ -1,10 +1,11 @@
-package lotto;
+package lotto.model;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
@@ -21,5 +22,19 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+    @Test
+    void numbersRangeTest() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 46)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void getNumbersTest() {
+        List<Integer> testNumbers = List.of(1, 2, 3, 4, 5, 6);
+        Lotto lotto = new Lotto(testNumbers);
+
+        List<Integer> testResultNumbers = lotto.getNumbers();
+
+        assertThat(testResultNumbers).isEqualTo(testNumbers);
+    }
 }
