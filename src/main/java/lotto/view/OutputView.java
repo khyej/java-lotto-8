84bfb21c4169ebc422de851fb2f8a404f1415d73
khyej <wl159753@naver.com>
2025-11-03 18:08:47 +0900

    feat: 로또 당첨 내역 출력

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -2,15 +2,33 @@ package lotto.view;
 
 import java.util.List;
 import lotto.model.Lotto;
+import lotto.model.LottoRank;
+import lotto.model.LottoStats;
 
 public class OutputView {
-    public void displayLottoCount(int count){
-        System.out.println(count+ "개를 구매했습니다.");
+    private static final List<LottoRank> DISPLAY_RANK = List.of(
+            LottoRank.FIFTH, LottoRank.FOURTH, LottoRank.THIRD, LottoRank.SECOND, LottoRank.FIRST
+    );
+
+    public void displayLottoCount(int count) {
+        System.out.println(count + "개를 구매했습니다.");
     }
 
-    public void displayLottos(List<Lotto> lottos){
-        for(Lotto lotto : lottos){
+    public void displayLottos(List<Lotto> lottos) {
+        for (Lotto lotto : lottos) {
             System.out.println(lotto.getNumbers());
+        }
+    }
+
+    public void displayStats(LottoStats lottoStats) {
+        System.out.println("당첨 통계");
+        System.out.println("---");
+        for (LottoRank lottoRank : DISPLAY_RANK) {
+            System.out.printf("%s - %d개",
+                    lottoRank.getDescription(),
+                    lottoStats.getCount(lottoRank)
+            );
+            System.out.println();
         }
     }
 }
